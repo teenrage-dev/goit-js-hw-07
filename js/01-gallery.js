a6267a29bcc97cf69dcae4b3009c4cc53a9c4cf8
Renat <72465313+teenrflagrage@users.noreply.github.com>
2022-05-20 18:00:49 +0300

    1 task done

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,81 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+const galleryBox = document.querySelector(".gallery");
+const galleryMarkup = createGallery(galleryItems);
 
-console.log(galleryItems);
+galleryBox.insertAdjacentHTML("afterbegin", galleryMarkup);
+galleryBox.addEventListener("click", onGalleryBoxClick)
+
+function createGallery(galleryItems) {
+    return galleryItems.map(({preview, original, description}) => {
+        return `
+            <div class="gallery__item">
+                <a href="${original}" class="gallery__link" onclick="event.preventDefault()">
+                    <img 
+                    src="${preview}" 
+                    data-source="${original}"
+                    alt="${description}" 
+                    class="gallery__image">
+                </a>
+            </div>
+`
+    })
+    .join('');
+};
+
+function onGalleryBoxClick(e){
+    const isImgEl = e.target.classList.contains("gallery__image");
+    if(!isImgEl){
+        return;
+    }
+    else{
+        const imagesCreate = basicLightbox.create(`
+            <img src="${e.target.dataset.source}">
+        `);
+        imagesCreate.show();
+    }
+}
+
+
+
+// function openImg(e){
+// for(let item of items){
+//     e.target
+//     const instance = basicLightbox.create(`
+//         <img src="${item.preview}" width="800" height="600">
+//     `)
+//         console.log(instance.show());
+//     };
+// }
+// console.log(openImg);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const instance = basicLightbox.create(`
+//     <img src="https://cdn.pixabay.com/photo/2019/05/14/16/43/himilayan-blue-poppy-4202825__340.jpg" width="800" height="600">
+// `)
+
+// instance.show()
