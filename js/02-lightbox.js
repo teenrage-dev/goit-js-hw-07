5f31ad8e6e3a527fbd54cab90fccf491c0e8bc98
Renat <72465313+teenrage-dev@users.noreply.github.com>
2022-05-20 23:43:59 +0300

    .reduce add links and tags to html

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,22 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+const galleryBox = document.querySelector(".gallery");
+const galleryMarkup = createGalleryItems(galleryItems);
 
-console.log(galleryItems);
+galleryBox.insertAdjacentHTML("afterbegin", galleryMarkup);
+
+function createGalleryItems(galleryItems){
+    galleryItems.reduce((acc, {preview, original, description}) => acc + `
+    <div class="gallery__item">
+        <a class="gallery__item" href="large-image.jpg">
+            <img 
+            class="gallery__image" 
+            src="${preview}" 
+            alt="${description}" />
+            data-source="${original}"
+        </a>
+    </div>
+    `
+, "")
+};
+console.log(galleryMarkup);
