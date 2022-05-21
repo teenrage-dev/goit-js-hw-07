7b2bdd9961a8e4cffd3378f349ab3aac30d802a6
Renat <72465313+teenrage-dev@users.noreply.github.com>
2022-05-21 20:11:53 +0300

    update task 2

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,22 +1,23 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 const galleryBox = document.querySelector(".gallery");
-const galleryMarkup = createGalleryItems(galleryItems);
-
+const galleryMarkup = createGallery(galleryItems);
 galleryBox.insertAdjacentHTML("afterbegin", galleryMarkup);
 
-function createGalleryItems(galleryItems){
-    galleryItems.reduce((acc, {preview, original, description}) => acc + `
+function createGallery(galleryItems) {
+    return galleryItems.reduce((acc, {preview, original, description}) => acc + `
     <div class="gallery__item">
         <a class="gallery__item" href="large-image.jpg">
             <img 
             class="gallery__image" 
             src="${preview}" 
-            alt="${description}" />
             data-source="${original}"
+            alt="${description}" />
         </a>
     </div>
     `
 , "")
 };
+
+
 console.log(galleryMarkup);
